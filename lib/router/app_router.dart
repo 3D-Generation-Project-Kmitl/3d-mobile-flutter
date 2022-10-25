29ce25d7fc0870fb7436651720e9f332be11408a
pjpure <pure254326@gmail.com>
2022-10-25 20:05:27 +0700

    add app router

---
+++ lib/router/app_router.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:e_commerce/presentation/screens/screens.dart';
+
+class AppRouter {
+  static Route onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case LoginScreen.routeName:
+        return LoginScreen.route();
+      case HomeScreen.routeName:
+        return HomeScreen.route();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route _errorRoute() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: '/error'),
+      builder: (_) => Scaffold(
+        appBar: AppBar(title: const Text('Error')),
+      ),
+    );
+  }
+}
