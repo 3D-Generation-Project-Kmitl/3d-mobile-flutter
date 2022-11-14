88590cb383b5d91abf8434ff9bfd4686029264ef
pholpaphankorn <phol12711@outlook.com>
2022-11-14 21:45:32 +0700

    added camera view

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,41 @@ import 'package:e_commerce/utils/dio_client.dart';
 
 import 'router/app_router.dart';
 
-import 'dart:async';
-import 'dart:io';
+// void main() {
 
-import 'package:camera/camera.dart';
+//   runApp(const MyApp());
+//   DioClient().init();
 
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiBlocProvider(
+//       providers: [
+//         BlocProvider<AuthCubit>(
+//           create: (context) => AuthCubit(),
+//         ),
+//         BlocProvider<UserCubit>(
+//           create: (context) => UserCubit(),
+//         ),
+//       ],
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         theme: AppTheme.lightAppTheme,
+//         onGenerateRoute: AppRouter.onGenerateRoute,
+//         home: const SplashScreen(),
+//       ),
+//     );
+//   }
+
+// }
 
 void main() {
-
   runApp(const MyApp());
   DioClient().init();
-
-
 }
 
 class MyApp extends StatelessWidget {
@@ -27,44 +50,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<AuthCubit>(
-          create: (context) => AuthCubit(),
-        ),
-        BlocProvider<UserCubit>(
-          create: (context) => UserCubit(),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.lightAppTheme,
-        onGenerateRoute: AppRouter.onGenerateRoute,
-        home: const SplashScreen(),
-      ),
+    return MaterialApp(
+      home:const CameraPage(),
     );
   }
-
 }
-
-// Future<void> main() async {
-//   // Ensure that plugin services are initialized so that `availableCameras()`
-//   // can be called before `runApp()`
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   // Obtain a list of the available cameras on the device.
-//   final cameras = await availableCameras();
-
-//   // Get a specific camera from the list of available cameras.
-//   final firstCamera = cameras.first;
-
-//   runApp(
-//     MaterialApp(
-//       theme: ThemeData.dark(),
-//       home: TakePictureScreen(
-//         // Pass the appropriate camera to the TakePictureScreen widget.
-//         camera: firstCamera,
-//       ),
-//     ),
-//   );
-// }
