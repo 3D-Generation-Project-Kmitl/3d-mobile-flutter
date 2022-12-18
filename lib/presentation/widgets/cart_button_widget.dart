9893e67a0ea8bfc2d2c1c967ea0a05642a36e10c
pjpure <pure254326@gmail.com>
2022-12-19 03:53:26 +0700

    update cart button

--- lib/presentation/widgets/cart_button_widget.dart
+++ lib/presentation/widgets/cart_button_widget.dart
@@ -28,6 +28,40 @@ class CartButton extends StatelessWidget {
                 size: 27,
               ),
             ),
+            BlocBuilder<CartCubit, CartState>(
+              builder: (context, state) {
+                if (state is CartLoaded) {
+                  if (state.carts.isEmpty) {
+                    return const SizedBox();
+                  }
+                  return Positioned(
+                    top: 8,
+                    right: 8,
+                    child: Container(
+                      padding: const EdgeInsets.all(2),
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        shape: BoxShape.circle,
+                      ),
+                      constraints: const BoxConstraints(
+                        minWidth: 15,
+                        minHeight: 15,
+                      ),
+                      child: Text(
+                        state.carts.length.toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 10,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  );
+                } else {
+                  return const SizedBox();
+                }
+              },
+            ),
           ],
         );
       },
